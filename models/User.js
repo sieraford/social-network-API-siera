d74603ca8f5f9fec1be21e475b8cb063ebc80e51
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2023-02-26 16:22:31 -0800

    set up MVC structure and creatr User schema

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// Construct a new instance of the schema class
+const userSchema = new Schema(
+    {
+        username: {
+          type: String,
+          required: true,
+        },
+        email: {
+          type: String,
+          required: true,
+        },
+        thoughts: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'thought',
+            },
+          ],
+        friends: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'user',
+            },
+          ],
+      },
+      {
+        toJSON: {
+          getters: true,
+          virtuals: true,
+        },
+      }
+)
+
+// Create a virtual property `friendCount` that gets the length of the user's friends array
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  })
+
+  // Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
